489410a1f3ba2c0a0735afeee8f55858eefe5266
赵广杰 <zhaoguangjie@retaileye.ai>
2020-11-10 14:10:54 +0800

    添加了csv,zip

---
+++ src/writer/CSVExport.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace data\writer;
+
+class CSVExport
+{
+    /**
+     * 表头字段
+     * @var array
+     */
+    private $header;
+
+    /**
+     * 打开文件指针
+     * @var resource
+     */
+    private $fp = null;
+
+    /**
+     * 根据文件获取指针
+     * @param $file
+     * @param string $mode 默认覆盖
+     */
+    public function __construct($file, $mode = 'w+')
+    {
+        /**
+         * locale -a|grep zh_CN如果不支持查看系统编码
+         * apt install language-pack-zh-hans
+         */
+        setlocale(LC_ALL, 'zh_CN.UTF-8');
+
+        $path = pathinfo($file, PATHINFO_DIRNAME);
+        $ext = pathinfo($file, PATHINFO_EXTENSION);
+        /**
+         * 目前支持csv格式
+         */
+        if ($ext == 'csv') {
+            if (!is_dir($path)) {
+                mkdir($path, 0775, true);
+            }
+            $this->fp = fopen($file, $mode);
+            /**
+             * UTF-8编码格式BOM头部
+             */
+            fwrite($this->fp, chr(0xEF) . chr(0xBB) . chr(0xBF));
+        }
+    }
+
+    /**
+     * 设置表头
+     * @param $header
+     */
+    public function setHeader($header)
+    {
+        $this->header = $header;
+        $content = implode(',', array_values($header)) . "\n";
+        fwrite($this->fp, $content);
+    }
+
+    /**
+     * 设置表内容
+     * @param $data
+     */
+    public function setContent($data)
+    {
+        $content = '';
+        foreach ($data as $row) {
+            foreach ($this->header as $key => $val) {
+                $content .= "$row[$key],";
+            }
+            $content = substr($content, 0, -1);
+            $content .= "\n";
+        }
+        fwrite($this->fp, $content);
+    }
+}
---
+++ src/writer/ZipExport.php
@@ -0,0 +1,41 @@
+<?php
+
+
+namespace data\writer;
+
+
+class ZipExport
+{
+    /**
+     * zip打包
+     * @param $folder
+     * @param $zipFile
+     */
+    public static function zip($folder, $zipFile)
+    {
+        // Get real path for our folder
+        $rootPath = realpath($folder);
+
+        // Initialize archive object
+        $zip = new ZipArchive();
+        $zip->open($zipFile, ZipArchive::CREATE | ZipArchive::OVERWRITE);
+
+        // Create recursive directory iterator
+        $files = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($rootPath), RecursiveIteratorIterator::LEAVES_ONLY);
+
+        foreach ($files as $name => $file) {
+            // Skip directories (they would be added automatically)
+            if (!$file->isDir()) {
+                // Get real and relative path for current file
+                $filePath = $file->getRealPath();
+                $relativePath = substr($filePath, strlen($rootPath) + 1);
+
+                // Add current file to archive
+                $zip->addFile($filePath, $relativePath);
+            }
+        }
+
+        // Zip archive will be created only after closing object
+        $zip->close();
+    }
+}
